39cb863baf7ea1780d8ddebfffe3ba0b8af4147a
RamezRayyan64 <Ramez.Rayyan64@gmail.com>
2023-05-30 11:22:16 +0430

    some bugs fixed

--- lib/story.dart
+++ lib/story.dart
@@ -1,10 +1,8 @@
 class Story {
-  late String storyTitle;
-  late String choice1;
-  late String choice2;
+  final String storyTitle;
+  final String choice1;
+  final String choice2;
 
   Story(
-      {required String storyTitle,
-      required String choice1,
-      required String choice2});
+      {required this.storyTitle, required this.choice1, required this.choice2});
 }
